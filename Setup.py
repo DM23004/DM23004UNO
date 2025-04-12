0128ccf39050ac42aa90524c370f9e0509310c4c
DM23004 <dm23004@ues.edu.sv>
2025-04-11 23:43:03 -0600

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type='text/markdown',
     author='Marvin',
     author_email='dm23004@ues.edu.sv',
-    url='https://github.com/DM23004/DM23004CDA',
+    url='https://github.com/DM23004/DM23004UNO',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
